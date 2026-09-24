30d4c5696dc32ca836098f585e8a7bfa6cfcf3e3
SANJAY U <115535475+USANJAY05@users.noreply.github.com>
2026-09-24 15:37:05 +0530

    feat: export feature group management helpers

--- src/platform/admin.js
+++ src/platform/admin.js
@@ -364,4 +364,5 @@ module.exports = {
   listOrganizations, listUsers, listAuditLog, getStats, getTimeSeries, getOrganizationDetail,
   getPricing, updatePricing, getFeatureFlags, updateFeatureFlag, deleteOrganization,
   listCostProviders, upsertCostProvider, listCostArchive,
+  getFeatureGroups, saveFeatureGroup, deleteFeatureGroup, sanitizeFeatureKeys,
 };
